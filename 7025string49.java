271e03917516ea81b7da476a57d7e656c9966d58
Vishnu9012 <149460884+Vishnu9012@users.noreply.github.com>
2023-11-03 11:40:35 +0530

    Add files via upload

---
+++ 7025string49.java
@@ -0,0 +1,9 @@
+ class A {
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("programming");
+	    System.out.println("string: " + sb);
+	   
+	    System.out.println("reverse: " + sb.reverse());
+	}
+}
+
